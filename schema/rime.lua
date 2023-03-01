b9d790551529a6ad7e619a8e44156275213c4caf
Yuhao Zhu, PhD, FRM <dr.yuhao.zhu@outlook.com>
2023-03-01 01:09:58 +0100

    更新方案，詞庫分成核心和拓展

--- schema/rime.lua
+++ schema/rime.lua
@@ -6,3 +6,4 @@ forfudan_freq_only = forfudan_freq_filter.forfudan_freq_only
 
 -- 全碼後置和全碼出單
 forfudan_single_char_only_for_full_code = require("forfudan_single_char_only_for_full_code")
+xuma_postpone_full_code = require("xuma_postpone_full_code")
