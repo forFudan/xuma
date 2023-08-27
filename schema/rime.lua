efd866fc108ed196f1028b328d896de646452c64
Yuhao Zhu, PhD, FRM <dr.yuhao.zhu@outlook.com>
2023-08-27 18:10:24 +0200

    Update the schema to fix wrong codes

--- schema/rime.lua
+++ schema/rime.lua
@@ -1,13 +1,13 @@
 -- rime.lua
 
 -- 過濾常用漢字
-local yuhao_char_filter = require("yuhao_char_filter")
-forfudan_freq_first = yuhao_char_filter.yuhao_char_first
-forfudan_freq_only = yuhao_char_filter.yuhao_char_only
+local yuhao_char_filter = require("xuma/yuhao_char_filter")
+yuhao_char_first = yuhao_char_filter.yuhao_char_first
+yuhao_char_only = yuhao_char_filter.yuhao_char_only
 
 -- 全碼後置和全碼出單
-forfudan_single_char_only_for_full_code = require("forfudan_single_char_only_for_full_code")
-xuma_postpone_full_code = require("xuma_postpone_full_code")
+forfudan_single_char_only_for_full_code = require("xuma/forfudan_single_char_only_for_full_code")
+xuma_postpone_full_code = require("xuma/xuma_postpone_full_code")
 
 -- 幫助
-forfudan_helper = require("forfudan_helper")
+forfudan_helper = require("xuma/forfudan_helper")
